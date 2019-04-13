f6bac87b14472750351791efa424e7142a841808
cosmos2028 <lestat290@hotmail.fr>
2019-04-13 12:56:04 +0200

    custm login

--- src/main/java/fr/glm/stopWaisting/securite/SecurityCofig.java
+++ src/main/java/fr/glm/stopWaisting/securite/SecurityCofig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -67,6 +68,16 @@ public class SecurityCofig extends WebSecurityConfigurerAdapter {
 		// jusqu'a la prochaine connexion
 		http.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).and().csrf().disable();
 
+	}
+
+	/**
+	 * supprimer la sécurité au niveau des access de pages
+	 */
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+
+		super.configure(web);
+		web.ignoring().antMatchers("/**");
 	}
 
 }
